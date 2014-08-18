cdd9f1d64e704babf812565d34b40e9213bf39bc
Wennio Kelson <wennio.kelson@dce.ufpb.br>
2014-08-18 20:35:52 -0300

    Task 1.04 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,6 +1,7 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
+import br.ufpb.dce.aps.coffeemachine.CoffeeMachineException;
 import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 
@@ -15,6 +16,9 @@ public class MyCoffeeMachine implements CoffeeMachine {
 	}
 
 	public void insertCoin(Coin dime) {
+		if(dime == null){
+			throw new CoffeeMachineException("Insert null coin");
+		}
 		total += dime.getValue();
 		fac.getDisplay().info("Total: US$ " + total/100 + "." + total%100);
 	}
