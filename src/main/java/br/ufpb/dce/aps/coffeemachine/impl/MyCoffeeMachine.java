2d468227d8fcb770320f403f29d196e2dfff1d24
Wennio Kelson <wennio.kelson@dce.ufpb.br>
2014-08-18 21:08:14 -0300

    Task 1.07 finished for OO

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,5 +1,7 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
+import java.util.ArrayList;
+
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachineException;
 import br.ufpb.dce.aps.coffeemachine.Coin;
@@ -10,27 +12,47 @@ public class MyCoffeeMachine implements CoffeeMachine {
 	
 	private ComponentsFactory fac;
 	private int total;
+	private ArrayList<Coin> coins = new ArrayList<Coin>();
 	
 	public MyCoffeeMachine(ComponentsFactory factory) {
 		fac = factory;
 		fac.getDisplay().info("Insert coins and select a drink!");
 	}
 
-	public void insertCoin(Coin dime) {
-		if(dime == null){
+	public void insertCoin(Coin moeda) {
+		if(moeda == null){
 			throw new CoffeeMachineException("Insert null coin");
 		}
-		total += dime.getValue();
+		total += moeda.getValue();
 		fac.getDisplay().info("Total: US$ " + total/100 + "." + total%100);
+		coins.add(moeda);
 	}
 
 	public void cancel() {
 		if (this.total == 0) {
 			throw new CoffeeMachineException(" Cancel without inserting coins");
+		}else if(coins.size() >= 2){
+			this.tarefaCompletaDevolverMoedas();
 		}
+		this.tarefaCompletaDevolverMoedas();
+	}
+	
+	//Meus métodos
+	
+	private void retornarMoedas(){
+		Coin[] reverso = Coin.reverse();
+		for (Coin r : reverso) {
+			for (Coin aux : this.coins) {
+				if (aux == r) {
+					this.fac.getCashBox().release(aux);
+				}
+			}
+		}
+	}
+	
+	private void tarefaCompletaDevolverMoedas(){
 		fac.getDisplay().warn(Messages.CANCEL_MESSAGE);
-		fac.getCashBox().release(Coin.halfDollar);
+		this.retornarMoedas();
 		fac.getDisplay().info(Messages.INSERT_COINS_MESSAGE);
 	}
-
 }
