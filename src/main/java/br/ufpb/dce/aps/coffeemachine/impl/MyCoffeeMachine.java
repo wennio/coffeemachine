fac5cb9a30f51b838adec5cff7671c81cfc7467d
Wennio Kelson <wennio.kelson@dce.ufpb.br>
2014-08-25 20:41:25 -0300

    Estudando 1.02

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,12 +1,25 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
+import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 
 public class MyCoffeeMachine implements CoffeeMachine{
 
+	//Variáveis
+	private ComponentsFactory factory;
+	private int dolar, cents;
+	
+	
 	public MyCoffeeMachine(ComponentsFactory factory) {
+		this.factory = factory; 
 		factory.getDisplay().info("Insert coins and select a drink!");
+	}
+
+	public void insertCoin(Coin dime) {
+		dolar = dime.getValue() / 100;
+		cents = dime.getValue() % 100;
+		factory.getDisplay().info("Total: US$ " + dolar + "." + cents + "");
 	}
 
 }
