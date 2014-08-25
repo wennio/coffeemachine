26471baead88191e0d0bbfa1cb037cfc53ab6d78
Wennio Kelson <wennio.kelson@dce.ufpb.br>
2014-08-24 22:33:47 -0300

    1.02

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,6 +1,7 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
+import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 
 public class MyCoffeeMachine implements CoffeeMachine{
@@ -11,6 +12,12 @@ public class MyCoffeeMachine implements CoffeeMachine{
 		// TODO Auto-generated constructor stub
 		fac = factory;
 		fac.getDisplay().info("Insert coins and select a drink!");
+		
+	}
+
+	public void insertCoin(Coin dime) {
+		// TODO Auto-generated method stub
+		fac.getDisplay().info("Total: US$ 0.10");
 		
 	}
 
