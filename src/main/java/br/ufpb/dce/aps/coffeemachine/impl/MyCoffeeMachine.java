c300910ddbec955ab9d0094e1f81fb82bbcde121
Wennio Kelson <wennio.kelson@dce.ufpb.br>
2014-08-25 21:43:49 -0300

    Estudando 1.07

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,5 +1,7 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
+import java.util.ArrayList;
+
 import br.ufpb.dce.aps.coffeemachine.CashBox;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachineException;
@@ -8,41 +10,52 @@ import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 import br.ufpb.dce.aps.coffeemachine.Display;
 import br.ufpb.dce.aps.coffeemachine.Messages;
 
-public class MyCoffeeMachine implements CoffeeMachine{
+public class MyCoffeeMachine implements CoffeeMachine {
 
-	//Variáveis
 	private ComponentsFactory factory;
-	private int dolar, cents;
+	private int cents, dolar; 
 	private Display display;
 	private CashBox cashBox;
+	private ArrayList<Coin> moedas;
 	
 	public MyCoffeeMachine(ComponentsFactory factory) {
-		dolar = 0;
 		cents = 0;
+		dolar = 0;
 		this.factory = factory;
-		this.display = factory.getDisplay();
+		this.display= factory.getDisplay();
 		this.cashBox = factory.getCashBox();
-		display.info("Insert coins and select a drink!");
+		this.moedas = new ArrayList<Coin>();
+		this.display.info("Insert coins and select a drink!");
 	}
 
-	public void insertCoin(Coin dime) {
-		if (dime != null){
-			dolar += dime.getValue() / 100;
-			cents += dime.getValue() % 100;
+	public void insertCoin(Coin coin) {
+		if (coin != null) {
+			moedas.add(coin);
+			cents += coin.getValue() % 100;
+			dolar += coin.getValue() / 100;
 			factory.getDisplay().info("Total: US$ " + dolar + "." + cents + "");
-		}else {
-			throw new CoffeeMachineException("Insert null coin");
+		} else {
+			throw new CoffeeMachineException("");
 		}
+
 	}
 
 	public void cancel() {
-		if((dolar == 0) && (cents == 0 )){
-			throw new CoffeeMachineException("Cancel without inserting coins");
-		}else {
-			display.warn(Messages.CANCEL_MESSAGE);
-			cashBox.release(Coin.halfDollar);
-			display.info(Messages.INSERT_COINS_MESSAGE);
+		if ((cents == 0) && (dolar == 0)){
+			throw new CoffeeMachineException("");
+		}
+		
+		if(moedas.size() > 0){
+			factory.getDisplay().warn(Messages.CANCEL_MESSAGE);
+			for(Coin coin : Coin.reverse()){
+				for(Coin aux : moedas){
+					if(aux == coin){
+						cashBox.release(aux);
+					}
+				}
+			}
+			factory.getDisplay().info(Messages.INSERT_COINS_MESSAGE);
 		}
 	}
-
+	
 }
