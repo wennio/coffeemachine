537c52dbb6ec175a7c564648caba5b331232b2fa
Wennio Kelson <wennio.kelson@dce.ufpb.br>
2014-08-25 22:05:28 -0300

    Estudando 1.09

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -8,7 +8,7 @@ import br.ufpb.dce.aps.coffeemachine.CoffeeMachineException;
 import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
 import br.ufpb.dce.aps.coffeemachine.Display;
-import br.ufpb.dce.aps.coffeemachine.Messages;
+import br.ufpb.dce.aps.coffeemachine.Drink;
 
 public class MyCoffeeMachine implements CoffeeMachine {
 
@@ -46,7 +46,7 @@ public class MyCoffeeMachine implements CoffeeMachine {
 		}
 		
 		if(moedas.size() > 0){
-			factory.getDisplay().warn(Messages.CANCEL_MESSAGE);
+			factory.getDisplay().warn("Cancelling drink. Please, get your coins.");
 			for(Coin coin : Coin.reverse()){
 				for(Coin aux : moedas){
 					if(aux == coin){
@@ -54,8 +54,22 @@ public class MyCoffeeMachine implements CoffeeMachine {
 					}
 				}
 			}
-			factory.getDisplay().info(Messages.INSERT_COINS_MESSAGE);
+			factory.getDisplay().info("Insert coins and select a drink!");
 		}
+	}
+
+	public void select(Drink drink) {
+		factory.getCupDispenser().contains(1);
+		factory.getWaterDispenser().contains(0.5);
+		factory.getCoffeePowderDispenser().contains (0.8); 
+		factory.getDisplay().info("Mixing ingredients.");	
+		factory.getCoffeePowderDispenser().release (0.6); 
+		factory.getWaterDispenser().release (0.9);
+		factory.getDisplay().info("Releasing drink.");
+		factory.getCupDispenser().release (1);
+		factory.getDrinkDispenser().release (0.3);
+		factory.getDisplay().info("Please, take your drink.");		
+		factory.getDisplay().info("Insert coins and select a drink!");
 	}
 	
 }
