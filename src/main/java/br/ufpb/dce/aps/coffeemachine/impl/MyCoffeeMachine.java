995b4f52605a891d61a3ef35c4cad68bdcd66665
Wennio Kelson <wennio.kelson@dce.ufpb.br>
2014-08-25 21:09:21 -0300

    Estudando 1.06

--- src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
+++ src/main/java/br/ufpb/dce/aps/coffeemachine/impl/MyCoffeeMachine.java
@@ -1,22 +1,28 @@
 package br.ufpb.dce.aps.coffeemachine.impl;
 
+import br.ufpb.dce.aps.coffeemachine.CashBox;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachine;
 import br.ufpb.dce.aps.coffeemachine.CoffeeMachineException;
 import br.ufpb.dce.aps.coffeemachine.Coin;
 import br.ufpb.dce.aps.coffeemachine.ComponentsFactory;
+import br.ufpb.dce.aps.coffeemachine.Display;
+import br.ufpb.dce.aps.coffeemachine.Messages;
 
 public class MyCoffeeMachine implements CoffeeMachine{
 
 	//Variáveis
 	private ComponentsFactory factory;
 	private int dolar, cents;
-	
+	private Display display;
+	private CashBox cashBox;
 	
 	public MyCoffeeMachine(ComponentsFactory factory) {
 		dolar = 0;
 		cents = 0;
-		this.factory = factory; 
-		factory.getDisplay().info("Insert coins and select a drink!");
+		this.factory = factory;
+		this.display = factory.getDisplay();
+		this.cashBox = factory.getCashBox();
+		display.info("Insert coins and select a drink!");
 	}
 
 	public void insertCoin(Coin dime) {
@@ -30,8 +36,12 @@ public class MyCoffeeMachine implements CoffeeMachine{
 	}
 
 	public void cancel() {
-		if((dolar == 0) || (cents == 0 )){
+		if((dolar == 0) && (cents == 0 )){
 			throw new CoffeeMachineException("Cancel without inserting coins");
+		}else {
+			display.warn(Messages.CANCEL_MESSAGE);
+			cashBox.release(Coin.halfDollar);
+			display.info(Messages.INSERT_COINS_MESSAGE);
 		}
 	}
 
